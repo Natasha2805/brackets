f5a5d24a4fabaf7ce9d042f714c7ae8385747ecc
Natasha2805 <59359497+Natasha2805@users.noreply.github.com>
2020-03-22 21:33:30 +0300

    feat: add complete part

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let leftCount = 0;
+    let rightCount = 0;
+
+    for (i = 0; i < str.length; i++) {
+        if (str[i] == '(') {
+            leftCount++;
+        } else if (str[i] == ')') {
+            rightCount++;
+        }
+    }
+    if (leftCount == rightCount) {
+        return true
+    } else {return false}
 }
