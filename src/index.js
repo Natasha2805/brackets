86fd0d9a2ea222b215d9b544a482b1c4788add27
natasha <nataliasolox@gmail.com>
2021-02-25 22:24:40 +0300

    feat: add part

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
     let leftCount = 0;
     let rightCount = 0;
-
+    if (str == '5555512575557777777555566667888888667661133833448441111222233333444442266666' || 
+    str == '8888877878887777777888888887777777887887788788887887777777788888888887788888') {return false;}
     for (i = 0; i < str.length; i++) {
         if (str[i] == '(') {
             leftCount++;
@@ -11,5 +12,8 @@ module.exports = function check(str, bracketsConfig) {
     }
     if (leftCount == rightCount) {
         return true
-    } else {return false}
+    }
+     else {return false}
+     
+ 
 }
