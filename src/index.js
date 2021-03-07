74e04005f4de37a80f4823bf8b0a2858326fba75
natasha <nataliasolox@gmail.com>
2021-03-07 16:02:58 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-    let leftCount = 0;
-    let rightCount = 0;
-    if (str == '5555512575557777777555566667888888667661133833448441111222233333444442266666' || 
-    str == '8888877878887777777888888887777777887887788788887887777777788888888887788888') {return false;}
-    for (i = 0; i < str.length; i++) {
-        if (str[i] == '(') {
-            leftCount++;
-        } else if (str[i] == ')') {
-            rightCount++;
-        }
-    }
-    if (leftCount == rightCount) {
-        return true
-    }
-     else {return false}
-     
- 
+     let newarr = str.split('');
+     let configBrackets = bracketsConfig;
+     for (let i = 0; i < newarr.length; i++) {
+         for (let j = 0; j < configBrackets.length; j++) {
+             if (newarr[i] === configBrackets[j][0] && newarr[i + 1] === configBrackets[j][1]) {
+                 newarr.splice(i, 2);
+                 if (newarr.length == 0) {
+                     return true;
+                 }
+                 i = -1;
+             }
+         }
+     }
+     return false;
 }
